7a6d32e8f73347a633eeb51233204a760fccfb51
vctrferreira <victtorferreiralima@gmail.com>
2017-09-26 14:00:17 -0300

    Use local db case redis broken

--- routers/bots.lua
+++ routers/bots.lua
@@ -5,8 +5,11 @@ local red = redis:new()
 red:set_timeout(1000)
 local ok, err = red:connect(os.getenv("REDIS_MASTER_ADDRESS"), tonumber(os.getenv("REDIS_MASTER_PORT")))
 if not ok then
-    ngx.say("failed to connect: ", err)
-    return
+    local ok, err = red:connect("127.0.0.1", "6379")
+    if not ok then
+        ngx.say("failed to connect: ", err)
+        return
+    end
 end
 
 red:select(tonumber(os.getenv("REDIS_MASTER_DB")))
